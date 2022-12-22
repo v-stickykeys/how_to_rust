7b2202e1cf4bacb097a150ef916898f66482068c
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2022-12-21 22:04:06 -0500

    feat: test raw pointer offsets

--- src/linked_lists/chaos.rs
+++ src/linked_lists/chaos.rs
@@ -1,12 +1,10 @@
-pub fn main() {
+pub fn wrong_order() {
     unsafe {
         let mut data = 10;
         let ref1 = &mut data;
         let ptr2 = ref1 as *mut _;
         // let ref2 = &mut *ref1;
         // let ref3 = &mut *ref2;
-
-
 
         *ref1 += 1;
         *ptr2 += 2;
@@ -20,12 +18,106 @@ pub fn main() {
     }
 }
 
+pub fn right_order() {
+    unsafe {
+        let mut data = 10;
+        let ref1 = &mut data;
+        let ptr2 = ref1 as *mut _;
+        let ref3 = &mut *ptr2;
+        let ptr4 = ref3 as *mut _;
+
+        // *ptr2 += 2;
+
+        *ptr4 += 4;
+        *ref3 += 3;
+        *ptr2 += 2;
+        *ref1 += 1;
+
+        println!("{}", data);
+    }
+}
+
+pub fn pointer_offset() {
+    unsafe {
+        let mut data = [0; 10];
+        let ref1_at_0 = &mut data[0];
+        let ptr2_at_0 = ref1_at_0 as *mut i32;
+        let ptr3_at_1 = ptr2_at_0.add(1);
+
+        *ptr3_at_1 += 3;
+        *ptr2_at_0 += 2;
+        *ref1_at_0 += 1;
+
+        // expect [3, 3, 0, ...]
+        println!("{:?}", &data[..]);
+    }
+}
+
+pub fn pointer_offset_copy() {
+    unsafe {
+        let mut data = [0; 10];
+        let ref1_at_0 = &mut data[0];
+        let ptr2_at_0 = ref1_at_0 as *mut i32;
+        let ptr3_at_0 = ptr2_at_0;
+
+        *ptr3_at_0 += 3;
+        *ptr2_at_0 += 2;
+        *ref1_at_0 += 1;
+
+        // expect [6, 0, 0, ...]
+        println!("{:?}", &data[..]);
+    }
+}
+
+pub fn mess() {
+    unsafe {
+        let mut data = [0; 10];
+        let ref1 = &mut data[0];
+        let ptr2 = ref1 as *mut i32;
+        let ptr3 = ptr2;
+        let ptr4 = ptr2.add(0);
+        let ptr5 = ptr3.add(1).sub(1);
+
+        *ptr3 += 3;
+        *ptr2 += 2;
+        *ptr4 += 4;
+        *ptr5 += 5;
+        *ptr3 += 3;
+        *ptr2 += 2;
+        *ref1 += 1;
+
+        // expect [20, 0, 0, ...]
+        println!("{:?}", &data[..]);
+
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use crate::linked_lists::chaos::main;
+    use super::*;
 
     #[test]
-    fn basics() {
-        main()
+    fn test_wrong_order() {
+        // wrong_order();
+    }
+
+    #[test]
+    fn test_right_order() {
+        right_order();
+    }
+
+    #[test]
+    fn test_pointer_offset() {
+        // pointer_offset();
+    }
+
+    #[test]
+    fn test_pointer_offset_copy() {
+        pointer_offset_copy();
+    }
+
+    #[test]
+    fn test_mess() {
+        mess();
     }
 }
