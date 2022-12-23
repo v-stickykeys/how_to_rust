756cf5b677aebd1f7ebed3da94c92e0d2f82d685
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2022-12-22 20:57:03 -0500

    feat: test multi allocation and slice pointers

--- src/linked_lists/chaos.rs
+++ src/linked_lists/chaos.rs
@@ -92,6 +92,58 @@ pub fn mess() {
     }
 }
 
+pub fn multi_allocation() {
+    unsafe {
+        let mut data = [0; 10];
+        let data_slice = &mut data[..];
+        let (slice_0, slice_1) = data_slice.split_at_mut(1);
+
+        let ref1_0 = &mut slice_0[0];
+        let ref2_1 = &mut slice_1[0];
+        let ptr3_0 = ref1_0 as *mut i32;
+        let ptr4_1 = ref2_1 as *mut i32;
+
+        *ptr4_1 += 4;
+        *ptr3_0 += 3;
+        *ref2_1 += 2;
+        *ref1_0 += 1;
+
+        // expect [6, 4, 0, ...]
+        println!("{:?}", &data[..]);
+    }
+}
+
+pub fn slice_as_pointer() -> [i32; 5] {
+    unsafe {
+        let mut data: [i32; 5] = [0; 5];
+        let slice_all = &mut data[..];
+        let ptr_all = slice_all.as_mut_ptr();
+
+        let ptr1_0 = ptr_all;
+        let ptr2_1 = ptr_all.add(1);
+        let ref3_0 = &mut *ptr1_0;
+        let ref4_1 = &mut *ptr2_1;
+
+        *ref4_1 += 4;
+        *ref3_0 += 3;
+        *ptr2_1 += 2;
+        *ptr1_0 += 1;
+
+        for idx in 0..5 {
+            *ptr_all.add(idx) += idx as i32;
+        }
+
+        for (idx, elem_ref) in slice_all.iter_mut().enumerate() {
+            *elem_ref += idx as i32;
+        }
+
+        // expect [6, 6, 4, 6, 8]
+        println!("{:?}", &data[..]);
+
+        data
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -119,5 +171,19 @@ mod test {
     #[test]
     fn test_mess() {
         mess();
+    }
+
+    #[test]
+    fn test_multi_allocation() {
+        multi_allocation();
+    }
+
+    fn test_slice_as_pointer() {
+        let data = slice_as_pointer();
+        assert_eq!(data[0], 6);
+        assert_eq!(data[1], 6);
+        assert_eq!(data[2], 4);
+        assert_eq!(data[3], 6);
+        assert_eq!(data[4], 8);
     }
 }
